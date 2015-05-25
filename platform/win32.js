5a6cf423589e6c09ae7f8325cf626069aa0645dc
Xavi <xavi.rmz@gmail.com>
2015-05-24 17:29:49 -0700

    Using path .join instead of string concats to build path to paste.vbs.

--- platform/win32.js
+++ platform/win32.js
@@ -1,7 +1,10 @@
 var iconv = require("iconv-lite");
+var path = require("path");
+
+var vbsPath = path.join(__dirname, ".\\fallbacks\\paste.vbs");
 
 exports.copy = { command: "clip", args: [] };
-exports.paste = { command: "cscript", args: [ "/Nologo", __dirname + ".\\fallbacks\\paste.vbs" ] };
+exports.paste = { command: "cscript", args: [ "/Nologo", vbsPath ] };
 exports.encode = function(str) { return iconv.encode(str, "utf16le"); };
 exports.decode = function(chunks) {
 	if(!Array.isArray(chunks)) { chunks = [ chunks ]; }

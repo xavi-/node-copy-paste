e8037f05b8a7b1175c81246a7f17007261b91683
Xavi <xavi.rmz@gmail.com>
2015-05-24 21:46:39 -0700

    Quoting paths on windows, incase there's a space in a folder name.

--- platform/win32.js
+++ platform/win32.js
@@ -1,7 +1,7 @@
 var iconv = require("iconv-lite");
 var path = require("path");
 
-var vbsPath = path.join(__dirname, ".\\fallbacks\\paste.vbs");
+var vbsPath = '"' + path.join(__dirname, ".\\fallbacks\\paste.vbs") + '"';
 
 exports.copy = { command: "clip", args: [] };
 exports.paste = { command: "cscript", args: [ "/Nologo", vbsPath ] };

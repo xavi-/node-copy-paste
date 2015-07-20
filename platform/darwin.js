002750075bbe9fcce19264db90dcc55e028909fa
Xavi <xavi.rmz@gmail.com>
2015-07-19 23:25:37 -0700

    Added full command property to deal with windows paths with quotes(') in them.

--- platform/darwin.js
+++ platform/darwin.js
@@ -1,5 +1,6 @@
 exports.copy = { command: "pbcopy", args: [] };
 exports.paste = { command: "pbpaste", args: [] };
+exports.paste.full_command = exports.paste.command;
 exports.encode = function(str) { return new Buffer(str, "utf8"); };
 exports.decode = function(chunks) {
 	if(!Array.isArray(chunks)) { chunks = [ chunks ]; }
--- platform/win32.js
+++ platform/win32.js
@@ -1,10 +1,14 @@
 var iconv = require("iconv-lite");
 var path = require("path");
 
-var vbsPath = '"' + path.join(__dirname, ".\\fallbacks\\paste.vbs") + '"';
+var vbsPath = path.join(__dirname, ".\\fallbacks\\paste.vbs");
+
+var paste = { command: "cscript", args: [ "/Nologo", vbsPath ] };
+paste.full_command = [ paste.command, paste.args[0], '"'+vbsPath+'"' ].join(" ");
 
 exports.copy = { command: "clip", args: [] };
-exports.paste = { command: "cscript", args: [ "/Nologo", vbsPath ] };
+exports.paste = paste;
+
 exports.encode = function(str) { return iconv.encode(str, "utf16le"); };
 exports.decode = function(chunks) {
 	if(!Array.isArray(chunks)) { chunks = [ chunks ]; }

88675659e83642ec2199bcbc59b91772a0833d97
Dennis Iversen <dennis.iversen@gmail.com>
2024-02-09 20:13:45 +0100

    Update linux.js
    Fix paste command. Fix all tests on WSL

--- platform/linux.js
+++ platform/linux.js
@@ -5,7 +5,7 @@ exports.copy =
 
 exports.paste =
 	process.env.WSL_DISTRO_NAME ?
-	{ command: "powershell.exe", args: ["-command", "Get-Clipboard"] } :
+	{ command: "powershell.exe", args: ['-noprofile', '-command', 'Get-Clipboard'] } :
 	{ command: "xclip", args: [ "-selection", "clipboard", "-o" ] };
   
 exports.paste.full_command = [ exports.paste.command ].concat(exports.paste.args).join(" ");
@@ -13,5 +13,12 @@ exports.encode = function(str) { return Buffer.from(str, "utf8"); };
 exports.decode = function(chunks) {
 	if(!Array.isArray(chunks)) { chunks = [ chunks ]; }
 
-	return Buffer.concat(chunks).toString("utf8");
+	var output = Buffer.concat(chunks).toString("utf8");
+
+	// Check if running under WSL and strip the last two characters added by powershell.exe
+	if (process.env.WSL_DISTRO_NAME) {
+		output = output.slice(0, -2);
+	}
+
+	return output;
 };

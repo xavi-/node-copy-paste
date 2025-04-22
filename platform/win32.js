a5d1bf30183d269fdb33d0d28b08006008f6fde6
Xavi Ramirez <xavi.rmz@gmail.com>
2025-04-22 20:36:58 +0800

    Applied linting to all files

--- platform/win32.js
+++ platform/win32.js
@@ -1,22 +1,21 @@
-var iconv = require("iconv-lite");
-var path = require("path");
+const iconv = require("iconv-lite");
+const path = require("node:path");
 
-var vbsPath = path.join(__dirname, ".\\fallbacks\\paste.vbs");
+const vbsPath = path.join(__dirname, ".\\fallbacks\\paste.vbs");
 
-var paste = { command: "cscript", args: [ "/Nologo", vbsPath ] };
-paste.full_command = [ paste.command, paste.args[0], '"'+vbsPath+'"' ].join(" ");
+const paste = { command: "cscript", args: ["/Nologo", vbsPath] };
+paste.full_command = [paste.command, paste.args[0], `"${vbsPath}"`].join(" ");
 
 exports.copy = { command: "clip", args: [] };
 exports.paste = paste;
 
-exports.encode = function(str) { return iconv.encode(str, "utf16le"); };
-exports.decode = function(chunks) {
-	if(!Array.isArray(chunks)) { chunks = [ chunks ]; }
+exports.encode = (str) => iconv.encode(str, "utf16le");
+exports.decode = (chunks) => {
+	const data = Array.isArray(chunks) ? chunks : [chunks];
 
-	var b64 = iconv.decode(Buffer.concat(chunks), "cp437");
+	let b64 = iconv.decode(Buffer.concat(data), "cp437");
 	b64 = b64.substring(0, b64.length - 2); // Chops off extra "\r\n"
 
-    // remove bom and decode
-    var result = Buffer.from(b64, "base64").subarray(3).toString("utf-8");
-    return result;
+	// remove bom and decode
+	return Buffer.from(b64, "base64").subarray(3).toString("utf-8");
 };
